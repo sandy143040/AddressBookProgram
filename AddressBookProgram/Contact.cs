e196c9ba741b6be756d25c9488c1bb80eb7af6ce
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-04-27 12:56:13 +0530

    [Sandeep] Modify Creating a contacts in Address Book

---
+++ AddressBookProgram/Contact.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProgram
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+
+        public List<Contact> contactsList = new List<Contact>();
+
+        public Contact(string firstName, string lastName, string address, string city, string state, int zip, int phone, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            City = city;
+            State = state;
+            Zip = zip;
+            Phone = phone;
+            Email = email;
+            contactsList.Add(this);
+        }
+        public void PrintContacts()
+        {
+            Console.WriteLine("\nContacts List:");
+            foreach (Contact contact in contactsList)
+            {
+                Console.WriteLine(" First Name:{0}\n Last Name: {1}\n Address:{2}\n City:{3}\n State:{4}\n Zip:{5}\n Phone:{6}\n Email:{7}\n", "result:" + contact.FirstName, contact.LastName, contact.Address, contact.City, contact.State, contact.Zip, contact.Phone, contact.Email);
+            }
+        }
+    }
+}
--- AddressBookProgram/Program.cs
+++ AddressBookProgram/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,30 @@ namespace AddressBookProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Problem");
+
+            Console.WriteLine("Enter the contact details:");
+            Console.Write("First Name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Address: ");
+            string address = Console.ReadLine();
+            Console.Write("City: ");
+            string city = Console.ReadLine();
+            Console.Write("State: ");
+            string state = Console.ReadLine();
+            Console.Write("Zip: ");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Phone: ");
+            int phone = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            // Create a new contact and add it to the list
+            Contact contact = new Contact(firstName, lastName, address, city, state, zip, phone, email);
+
+            // Print out the contacts in the list
+            contact.PrintContacts();
             Console.ReadLine();
         }
     }
