149a69caf7c58cd13d47e4f3dde78e1b2310ce28
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-04-27 16:36:51 +0530

    [Sandeep] Modify edit existing contact

--- AddressBookProgram/Program.cs
+++ AddressBookProgram/Program.cs
@@ -15,28 +15,79 @@ namespace AddressBookProgram
 
             AddressBook addressBook = new AddressBook();
 
-            Console.WriteLine("Add a new contact to Address Book:");
-            Console.Write("Enter First Name: ");
-            string firstName = Console.ReadLine();
-            Console.Write("Enter Last Name: ");
-            string lastName = Console.ReadLine();
-            Console.Write("Enter Address: ");
-            string address = Console.ReadLine();
-            Console.Write("Enter City: ");
-            string city = Console.ReadLine();
-            Console.Write("Enter State: ");
-            string state = Console.ReadLine();
-            Console.Write("Enter Zip: ");
-            int zip = int.Parse(Console.ReadLine());
-            Console.Write("Enter Phone Number: ");
-            int phone = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter Email: ");
-            string email = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("Select an option:");
+                Console.WriteLine("1. Add contact");
+                Console.WriteLine("2. Edit contact");
+                Console.WriteLine("3. Exit");
 
-            Contact newContact = new Contact(firstName, lastName, address, city, state, zip, phone, email);
-            addressBook.AddContact(newContact);
-            addressBook.PrintAllContacts();
-            Console.ReadLine();
+                int option = Convert.ToInt32(Console.ReadLine());
+              
+                switch (option)
+                {
+                    case 1:
+                        ContactPerson newContact = new ContactPerson();
+                        Console.WriteLine("Enter first name:");
+                        newContact.FirstName = Console.ReadLine();
+                        Console.WriteLine("Enter last name:");
+                        newContact.LastName = Console.ReadLine();
+                        Console.WriteLine("Enter address:");
+                        newContact.Address = Console.ReadLine();
+                        Console.WriteLine("Enter city:");
+                        newContact.City = Console.ReadLine();
+                        Console.WriteLine("Enter state:");
+                        newContact.State = Console.ReadLine();
+                        Console.WriteLine("Enter zip code:");
+                        newContact.Zip = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter phone number:");
+                        newContact.PhoneNumber = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter email address:");
+                        newContact.Email = Console.ReadLine();
+                        addressBook.AddContact(newContact);
+                        Console.WriteLine("Contact added.");
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter first name of contact to edit:");
+                        string firstName = Console.ReadLine();
+                        Console.WriteLine("Enter last name of contact to edit:");
+                        string lastName = Console.ReadLine();
+                        ContactPerson existingContact = addressBook.FindContact(firstName, lastName);
+                        if (existingContact == null)
+                        {
+                            Console.WriteLine("Contact not found.");
+                            break;
+                        }
+                        ContactPerson updatedContact = new ContactPerson();
+                        Console.WriteLine("Enter new first name (leave blank to keep current value):");
+                        updatedContact.FirstName = Console.ReadLine();
+                        Console.WriteLine("Enter new last name (leave blank to keep current value):");
+                        updatedContact.LastName = Console.ReadLine();
+                        Console.WriteLine("Enter new address (leave blank to keep current value):");
+                        updatedContact.Address = Console.ReadLine();
+                        Console.WriteLine("Enter new city (leave blank to keep current value):");
+                        updatedContact.City = Console.ReadLine();
+                        Console.WriteLine("Enter new state (leave blank to keep current value):");
+                        updatedContact.State = Console.ReadLine();
+                        Console.WriteLine("Enter new zip code (leave blank to keep current value):");
+                        updatedContact.Zip = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter new phone number (leave blank to keep current value):");
+                        updatedContact.PhoneNumber = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter new email address (leave blank to keep current value):");
+                        updatedContact.Email = Console.ReadLine();
+                        addressBook.EditContact(firstName, lastName, updatedContact);
+                        Console.WriteLine("Contact updated.");
+                        break;
+                    case 3:
+                        Console.WriteLine("Exiting program.");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option selected.");
+                        break;
+                     
+                }
+                   Console.ReadLine();
+            }
         }
     }
 }
